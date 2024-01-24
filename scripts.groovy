28cdff543a064424d432d7ce56fb9197f789e55e
vazand <vazandvel@gmail.com>
2024-01-24 16:08:01 +0530

    working model - external scripts

--- scripts.groovy
+++ scripts.groovy
@@ -2,7 +2,7 @@ def extScripts(){
     echo "This line is printed by running this mehtod!"
 }
 def usingEnvs(){
-    echo "My name is ${params.MY_NAME}.(used params.MYNAME to get my name here)"
+    echo "My name is ${env.MY_NAME}.(used env.MYNAME to get my name here)"
 }
 def printMessage(message){
     println("the message you provided is ---- ${message}")
